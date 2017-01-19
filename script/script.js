9ac007b6a5220d30d55b289a1c59f4b2d2e5515c
Krzysztof Hamerszmit <kkhamer@gmail.com>
2017-01-19 12:56:32 +0100

    Add carousel arrows

--- script/script.js
+++ script/script.js
@@ -1,12 +1,10 @@
-// alert('hej')
-
 $(function(){
 	var carouselList = $('#carousel ul');
 
-	setInterval(changeSlide, 3000);
+	setInterval(changeSlide, 5000);
 
 	function changeSlide() {
-		carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);
+		carouselList.animate({'marginLeft': -400}, 1000, moveFirstSlide);
 	};
 
 	function moveFirstSlide() {
@@ -18,3 +16,10 @@ $(function(){
 	};
 
 })
+
+var carouselMenuLeft = $('.carousel-cont::before');
+
+//Dorób po bokach przyciski służące do zmiany slajdu.
+
+//Dodaj kontrolki (mogą być kółka) wskazujące który slajd jest aktualnie wyświetlany (np aktualny slajd to pełne kółko a pozostałe są puste).
+//Zrób tak żeby kliknięcie w kontrolkę przewijało karuzelę tak by widoczny był slajd do niej przypisany.
